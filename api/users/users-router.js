753151bfe4183d3e8f4495b1231e872544cf7cbd
LuiCoro <luiacorona029@icloud.com>
2021-12-01 14:38:01 -0800

    'THIS IS HOW ALL PROJECTS SHOULD BE SETUP' [ |~| STEP |~| ONE |~| ] - create your auth middleware then console log them to test if they are connected correctly which it is - this is to be used the exact same way as middleware and basically acts the same - [ |~| IMPORTANT |~| ] spelling is a must and typos should  be avoided at all costs!

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,13 +1,14 @@
 const router = require("express").Router()
+const {restricted} = require('../auth/auth-middleware')
 
 const Users = require("./users-model.js")
 
-router.get("/", (req, res, next) => {
+router.get("/", restricted, (req, res, next) => {
   Users.find()
-    .then(users => {
-      res.status(200).json(users)
-    })
-    .catch(next)
+      .then(users => {
+        res.status(200).json(users)
+      })
+      .catch(next)
 })
 
 module.exports = router
