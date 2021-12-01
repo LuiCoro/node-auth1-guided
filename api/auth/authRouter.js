c05709c32b802c5aaa8d8290155f765135fcf5bd
LuiCoro <luiacorona029@icloud.com>
2021-12-01 15:10:57 -0800

    'THIS IS HOW ALL PROJECTS SHOULD BE SETUP' [ |~| STEP |~| TWO |~| ] - created a validatePayLoad function - to first create a user then you'll be able to see the info in the req.body

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
 
-router.post('/register', async (req, res, next) => {
-  res.json('register wired!')
+// |~| STEP |~| TWO |~|
+// [ |~| CREATING |~| ]
+// - A -> function that validates if the user is auth or not
+const validatePayLoad = (req, res, next) => {
+  next()
+}
+
+
+router.post('/register', validatePayLoad, async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    // [ |~| DO |~| NOT |~| REMOVE |~| COMMENT |~| ]
+    // do this to check if the stuff you need is actually passing through
+    // console.log(username, password)
+    res.json({username, password})
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validatePayLoad, async (req, res, next) => {
   res.json('login wired!')
 
 })
