df5b6d7314f299c6b29e42a874b487d80bbf1ccc
LuiCoro <luiacorona029@icloud.com>
2021-12-01 17:48:24 -0800

    'THIS IS HOW ALL PROJECTS SHOULD BE SETUP' [ |~| STEP |~| EIGHT |~| ] - restricted function is refactored to have session stored inside it to make sure if the cookie expires the current user at the time cannot view the users

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,8 +1,18 @@
-function restricted(req, res, next) {
-  console.log('restricted access to authed users only!!')
-  next()
-}
+// function restricted(req, res, next) {
+//   console.log('restricted access to authed users only!!')
+//   next()
+// }
 
+// STEP 8
+function restricted(req, res, next) {
+  if(req.session.user) {
+    next()
+  } else {
+    next({
+      status: 401, message: 'BAD CREDENTIALS'
+    })
+  }
+}
 
 module.exports = {
   restricted
